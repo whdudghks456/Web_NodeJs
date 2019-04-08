28dc5cc1a11e792b596e1977d426cf764ce64ee8
Mangbo <whdudghks45629@gmail.com>
2019-04-08 11:11:04 +0900

    commit

--- be/app.js
+++ be/app.js
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, '../', 'fe', 'dist')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
+// commit
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
