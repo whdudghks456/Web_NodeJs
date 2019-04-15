72bc233b4e7813e2902628a1ccb9a2592035f51e
Mangbo <whdudghks45629@gmail.com>
2019-04-15 11:05:36 +0900

    fix_user

---
+++ fe/src/views/user.vue
@@ -0,0 +1,63 @@
+<template>
+  <v-container grid-list-md text-xs-center>
+    <v-layout>
+      <v-flex xs12 sm6 md4 v-for="u in users">
+        <v-card>
+            <v-img
+            src="https://cdn.vuetifyjs.com/images/cards/desert.jpg"
+            aspect-ratio="2.75"
+            ></v-img>
+
+            <v-card-title primary-title>
+            <div>
+                <h3 class="headline mb-0">{{u.name}}</h3>
+                <div> {{u.age}} </div>
+            </div>
+            </v-card-title>
+
+            <v-card-actions>
+            <v-btn flat color="orange">Share</v-btn>
+            <v-btn flat color="orange">Explore</v-btn>
+            </v-card-actions>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+<script>
+import axios from 'axios'
+export default {
+    data() {
+        return {
+            users: []
+        }
+    },
+    mounted () {
+        axios.get('http://localhost:3000/api/user')
+        .then((r) => {
+            this.users = r.data.users
+            console.log(r)
+        })
+        .catch((e) => {
+            console.error(e.message)
+        })
+    }
+}
+</script>
+// <!-- Add "scoped" attribute to limit CSS to this component only -->s
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
