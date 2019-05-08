c1d402a542b1de54fa89b43ab2c5d973f0be0dcb
Mangbo <whdudghks45629@gmail.com>
2019-05-09 08:47:50 +0900

    modified

--- be/routes/api/user/index.js
+++ be/routes/api/user/index.js
@@ -1,9 +1,7 @@
-var createError = require('http-errors');
 var express = require('express');
+var createError = require('http-errors');
 var router = express.Router();
 const User = require('../../../models/users')
-
-/* GET home page. */
 
 
 // router.get('/', function(req, res, next) {
@@ -20,56 +18,72 @@ const User = require('../../../models/users')
 //   res.send({ users: us })
 // });
 
-router.get('/', function(req, res, next) {
-  User.find()
-    .then(r => {
-      res.send({ success : true, users: r})
-    })
-    .catch(e => {
-      res.send({ sucess : false})
-    })
-})
 
-router.post('/', function(req, res, next) {
+module.exports = router;
+
+  //Restful
+  router.get('/', function(req, res, next) {
+
+    User.find()
+      .then(r => {
+        res.send({ success : true, users: r})
+      })
+      .catch(e => {
+        res.send({ success : false})
+      })
+
+  // console.log(req.query)
+  // console.log(req.body)
+  //
+  // res.send({ users: us })
+});
+
+router.post('/', (req, res, next) => {
+
   const { name, age } = req.body
   const u = new User({ name, age })
     u.save()
       .then(r => {
-        res.send({ success: true, msg: r})
+        res.send({ success: true, msg: r })
       })
       .catch(e => {
         res.send({ success: false, msg: e.message })
       })
+
+  // console.log(req.query)
+  // console.log(req.body)
+  // res.send({ success: true, msg: 'post ok' })
 })
 
-// router.get('/', function(req, res, next) {
-//   console.log(req.query);
-//   console.log(req.body);
+router.put('/:id', (req, res, next) => {
+  const id = req.params.id
+  const { name, age } = req.body
+  User.updateOne({ _id: id }, { $set: { name, age }})
+    .then(r => {
+      res.send({ success: true, msg: r })
+    })
+    .catch(e => {
+      res.send({ success: false, msg: e.message })
+    })
+  // res.send({ success: true, msg: 'put ok' })
+})
 
-//   res.send({users: us});
-// });
 
-// router.post('/', (req, res, next) => {
-//   console.log(req.query);
-//   console.log(req.body);
-//   res.send({ success: true, msg: 'post ok' })
-// });
-
-router.put('/', (req, res, next) => {
-  console.log(req.query);
-  console.log(req.body);
-  res.send({ success: true, msg: 'put ok' })
-});
-
-router.delete('/', (req, res, next) => {
-  console.log(req.query);
-  console.log(req.body);
+router.delete('/:id', (req, res, next) => {
+  const id = req.params.id
+  User.deleteOne({ _id: id })
+    .then(r => {
+      res.send({ success: true, msg: r })
+    })
+    .catch(e => {
+      res.send({ success: false, msg: e.message })
+    })
   res.send({ success: true, msg: 'del ok' })
-});
+})
 
-// sdfd
+
 router.all('*', function(req, res, next) {
-  next(createError(404,'API를 찾을 수 없습니다.'));
+  next(createError(404, 'API를 찾을 수 없습니다.'));
 });
 
 module.exports = router;
